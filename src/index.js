cfdb73a830ac8ec981d55ee2ae92b2ff516c2a2e
edwardlr95 <edward9512@gmail.com>
2021-01-18 03:07:19 -0600

    creacion eslintrc y eslintignore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
 // Script de Frontend
-console.log("Modern JS Working!!! thanks babel....");
-let show = (message= "Saludo por defecto") =>{
-alert (message)
-}
-//Ejemplo
-show()
-show("Mi saludo")
+console.log('Modern JS Working!!! thanks babel....');
+const show = (message = 'Saludo por defecto') => {
+  alert(message);
+};
+// Ejemplo
+show();
+show('Mi saludo');

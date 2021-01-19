4bc0ce95a7269f9e66f96485c64eb45a95f9dd18
edwardlr95 <edward9512@gmail.com>
2021-01-19 09:24:23 -0600

    materialize working testing with colors

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@
 /* eslint-disable no-console */
 // Script de Frontend
 // Importar los estilos
-import './styles/styles.css';
+// import './styles/styles.css';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize';
-
-console.log('Modern JS Working!!! thanks babel....');
-// const show = (message = 'Saludo por defecto') => {
-// alert(message);
-// };
